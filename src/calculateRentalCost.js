f1e5b8d238e96f28a97a5ed7c87c420bf38fcfe9
Toni_Adrenalin <toni.toni@adrenalinmedia.com.au>
2025-02-21 17:27:02 +1100

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,21 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const LONG_TERM = 7;
+  const MID_TERM = 3;
+  const LONG_TERM_DISCOUNT = 50;
+  const MID_TERM_DISCOUNT = 20;
+  const BASE_PRICE = 40;
+
+  if (days >= LONG_TERM) {
+    return BASE_PRICE * days - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MID_TERM) {
+    return BASE_PRICE * days - MID_TERM_DISCOUNT;
+  }
+
+  return BASE_PRICE * days;
 }
 
 module.exports = calculateRentalCost;
